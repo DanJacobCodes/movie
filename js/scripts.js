c20b54b18ccc0ec2fa2aa2c1b5c25393822f9c70
Ben Metzger and Dan Jacob <ben@>
2017-04-04 10:34:32 -0700

    add branching and object

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,51 @@
 // Your constructor and prototype could be called Ticket and you can come up with the formula for determining how the price is calculated depending on the input from the user.
 
 //business logic
+function Ticket(newRelease, matinee, age) {
+  this.newRelease = newRelease;
+  this.matinee = matinee;
+  this.age = age;
+}
+
+function calculateCost(newRelease, matinee, age) {
+  debugger;
+  var cost = 8;
+  if (newRelease === "newRelease") {
+    cost += 3;
+  }
+  if (matinee === "matinee") {
+    cost -= 2;
+  }
+  if (age === "adult") {
+    cost += 3.5;
+  }
+  return cost;
+}
+
+function resetFields() {
+    $('input[type="checkbox"]').prop('checked',false);
+    $("#age").val("adult")
+}
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("#form").submit(function(event) {
+    // note - refactor if time
+    var newRelease = "";
+    var matinee = "";
+    $("input:checkbox[name=newRelease]:checked").each(function() {
+      newRelease = $(this).val()
+    })
+    $("input:checkbox[name=matinee]:checked").each(function() {
+      matinee = $(this).val()
+    })
+    var age = $("#age").val();
+    var newTicket = new Ticket(newRelease, matinee, age);
+    debugger;
+    var cost = calculateCost(newTicket.newRelease, newTicket.matinee, newTicket.age);
+    $("#cost").show();
+    $("#cost h2").text(cost);
     event.preventDefault();
+    resetFields();
   });
 });
